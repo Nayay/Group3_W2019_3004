15020b730aaca8bbf63b1c20e5b406f74607bea0
MacStudent <macstudent@126-iMac18.local>
2019-02-22 18:47:27 -0500

    Extension and User Class

--- Group3_W2019_3004/Extension/ExtensionOrders.swift
+++ Group3_W2019_3004/Extension/ExtensionOrders.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+extension String
+    
+{
+    
+    //Valid email address
+    
+    func isValidEmail() -> Bool
+        
+    {
+        
+        // print("validate calendar: \(testStr)")
+        
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        
+        return emailTest.evaluate(with: self)
+        
+    }
+    
+    //Valid password
+    
+    func isValidPassword() -> Bool {
+        
+        //guard self != nil else { return false }
+        
+        // at least one uppercase,
+        
+        // at least one digit
+        
+        // at least one lowercase
+        
+        // 8 characters total
+        
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}")
+        
+        return passwordTest.evaluate(with: self)
+        
+    }
+    
+}
--- Group3_W2019_3004/Users/User.swift
+++ Group3_W2019_3004/Users/User.swift
@@ -7,3 +7,100 @@
 //
 
 import Foundation
+
+class User{
+
+    var userID:String?
+    var password:String?
+ 
+    init()
+        
+    {
+        
+        self.userID = String()
+        
+        self.password = String()
+        
+    }
+    
+    init?(userID: String, password: String)
+        
+    {
+        
+        self.userID = userID
+        
+        self.password = password
+        
+        
+        
+        if (userID.isEmpty || password.isEmpty)
+            
+        {
+            
+            if(!password.isValidPassword())
+                
+            {
+                
+                print("Invalid Password format")
+                
+                return nil
+                
+            }
+            
+            
+            
+        }else{
+            
+            print("User ID or Password cannot be empty ")
+            
+            return nil
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+   
+    
+    
+    func verifyLogin()-> Bool
+        
+    {
+        
+        if(true)
+            
+        {
+            
+            return false
+            
+        }
+            
+        else{
+            
+            
+            
+            return true
+            
+        }
+        
+    }
+    
+    
+    
+    func display()
+        
+    {
+        
+        print(self.userID!,self.password!)
+        
+    }
+    
+    
+    
+    
+    
+}
+
