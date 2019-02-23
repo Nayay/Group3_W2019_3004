66d95e3def1b763709529441754fe41ae607dc4b
MacStudent <macstudent@126-imac19.local>
2019-02-22 20:04:50 -0500

    Shipping Info

--- Group3_W2019_3004/Shipping/ShippingInfo.swift
+++ Group3_W2019_3004/Shipping/ShippingInfo.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+class  ShoppingInfo{
+    var shippingID:Int?
+    var shippingType:String?
+    var shippingCost:String?
+    var shippingRegionID:String?
+     init()
+    {
+        self.shippingID = 0
+        self.shippingType =  String()
+        self.shippingCost =  String()
+        self.shippingRegionID = String()
+     }
+    
+    init(shippingID: Int, shippingType: String, shippingCost: String, shippingRegionID: String)
+    {
+        self.shippingID = shippingID
+        self.shippingType = shippingType
+        self.shippingCost =  shippingCost
+        self.shippingRegionID = shippingRegionID
+        
+    }
+    
+    func display() {
+        print(self.shippingID!,self.shippingType!,self.shippingCost!,self.shippingRegionID!)
+        
+    }
+    
+    func updateShippingInfo(){
+        
+        
+    }
+    
+}
