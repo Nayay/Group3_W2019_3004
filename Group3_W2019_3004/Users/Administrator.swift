242509cf74db6d624cf38963ac515d86b86e3dd6
MacStudent <macstudent@126-imac17.local>
2019-02-25 18:48:35 -0500

    admin class

--- Group3_W2019_3004/Users/Administrator.swift
+++ Group3_W2019_3004/Users/Administrator.swift
@@ -13,36 +13,30 @@ class Administrator:User
     var adminName:String?
     var email:String?
     
-    override init()
-        {
-        super.init()
-        self.adminName = String()
-        self.email = String()
-        }
-    init?(userID: String, password: String, adminName: String, email: String)
-        {
-        super.init(userID: userID, password: password)
+    
+    init?(userID: String, password: String, adminName: String, email: String) throws
+    {
+        try super.init(userID: userID, password: password)
         self.adminName = adminName
         self.email = email
         if !email.isValidEmail()
-            {
+        {
             print("Invalid Admin Email ID")
             return nil
-            }
+        }
         if adminName.count < 4
-            {
+        {
             print("Admin Name must be greater 4 digits")
             return nil
-            }
         }
+    }
     override func display(){
-    
-        super.display()
         
-        print(self.adminName!, self.email!)
-        }
-func updateCatalog()->Bool{
+        super.display()
+        print("Administrator Name: \(self.adminName!) E-masl: \(self.email!)")
+    }
+    func updateCatalog(products: [Product], updProduct:Product)->Bool{
         return true
-        }
+    }
 }
 
